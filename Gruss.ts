70e4191cc530e045594faa6712ac43f29f8e2c8b
User <User@User-PC.fritz.box>
2017-03-16 17:42:38 +0100

    Loading some work up.

--- Gruss.ts
+++ Gruss.ts
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var person;
+    var person: string;
     person = prompt("Gib hier deinen Namen ein.");
     if (person != null) {
         document.getElementsByTagName("div")[0].innerHTML = "Guten Tag und willkommen auf Nadias Seite " + person;
