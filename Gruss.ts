7f8139ba23f2394dbe54548962162c2a56afc37b
Nadia Credidio <nadia.credidio@gmx.de>
2017-03-16 16:44:49 +0100

    Update Gruss.ts
    a

--- Gruss.ts
+++ Gruss.ts
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var person;
     person = prompt("Gib hier deinen Namen ein.");
     if (person != null) {
-        document.getElementsByTagName("div")[0].innerHTML = "Guten Tag und willkommen auf Nadias Seite " + name;
+        document.getElementsByTagName("div")[0].innerHTML = "Guten Tag und willkommen auf Nadias Seite " + person;
     }
 });
