d518070a2e97ac562728631677203da7b1f0af85
User <User@User-PC.fritz.box>
2017-04-09 17:53:34 +0200

    Ha hochladen

---
+++ aufg3/Mau.js
@@ -0,0 +1,51 @@
+//In Zusammenarbeit mit Sara Kalinic, Christian Saile und Janett St�we die Lehrerin gespielt hat :P
+document.addEventListener("DOMContentLoaded", function () {
+    //Array anlegen
+    let kartenSammlung = ['Pik 7', 'Pik 8', 'Pik 9',
+        'Pik 10', 'Pik Bube', 'Pik Dame',
+        'Pik K�nig', 'Pik As',
+        'Kreuz 7', 'Kreuz 8', 'Kreuz 9',
+        'Kreuz 10', 'Kreuz Bube', 'Kreuz Dame',
+        'Kreuz K�nig', 'Kreuz As',
+        'Karo 7', 'Karo 8', 'Karo 9',
+        'Karo 10', 'Karo Bube', 'Karo Dame',
+        'Karo K�nig', 'Karo As',
+        'Herz 7', 'Herz 8', 'Herz 9',
+        'Herz 10', 'Herz Bube', 'Herz Dame',
+        'Herz K�nig', 'Herz As'];
+    // Nachziehstapel (deck) 
+    let nachziehStapel = document.getElementById("deck");
+    //Eventlistener auf den Nachziehstapel
+    nachziehStapel.addEventListener("click", function () {
+        //Zufallskarte generieren
+        let zufallsKarte = kartenSammlung.splice(Math.round(Math.random() * (kartenSammlung.length - 1)), 1)[0];
+        // Variable f�r funktion anlegen
+        let leerKarte = sucheLeerKarte();
+        function sucheLeerKarte() {
+            for (let i = 0; i < handKarten.length; i++) {
+                if (handKarten[i].textContent === "") {
+                    return handKarten[i];
+                }
+            }
+            return null;
+        }
+        if (leerKarte != null) {
+            leerKarte.innerText = zufallsKarte;
+        }
+        if (kartenSammlung.length === 0) {
+            nachziehStapel.innerText = "leer";
+        }
+    });
+    // ABLAGE 
+    let ablage = document.getElementById("discard");
+    let handKarten = document.getElementsByClassName("Handkarten");
+    for (let v = 0; v < handKarten.length; v++) {
+        //Karten von Hand in die Ablage
+        handKarten[v].addEventListener("click", function (event) {
+            let karteHand = event.target;
+            ablage.textContent = karteHand.textContent;
+            karteHand.textContent = "";
+        });
+    }
+});
+//# sourceMappingURL=Mau.js.map
---
+++ aufg3/SchachMark.ts
@@ -0,0 +1,76 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let sum: number = 0;
+    let rice: number = 1;
+    let line: number = 0;
+    let element: HTMLElement;
+    
+    for (let n: number = 0; n < 64; n++) {
+        element = document.createElement("div");
+        if (((n + line) % 2) == 0) {
+            element.className = "board black";
+        }
+        else {
+            element.className = "board white";
+        }
+        if (((n + 1) % 8) == 0) {
+            line++;
+        }
+        element.innerText = "" + rice;
+        rice *= 2;
+        document.body.appendChild(element);
+
+
+        if (n < 8) {
+            element.addEventListener("click", mark);
+        }
+    }
+
+    let mouseBox: HTMLDivElement = document.createElement("div");
+    document.body.appendChild(mouseBox);
+    mouseBox.className = "mouseBox"; // 
+    document.addEventListener("mousemove", boxbewegen);
+
+
+    function boxbewegen(Event: MouseEvent) {
+        mouseBox.style.left = (Event.clientX + 30) + "px";
+        mouseBox.style.top = (Event.clientY + 30) + "px";
+    }
+
+
+    function mark(event: MouseEvent) {
+        let target: HTMLDivElement = <HTMLDivElement>event.target;
+
+        if (target.className.includes("marked")) {
+
+            if (target.className.includes("black")) {
+                target.className = "board black";
+            }
+            else {
+                target.className = "board white";
+            }
+        }
+
+        else {
+            target.className += " marked";
+        }
+
+        sum = 0;
+        let marked: NodeListOf<HTMLElement> = <NodeListOf<HTMLElement>>document.getElementsByClassName("marked");
+
+        for (let i: number = 0; i < marked.length; i++) {
+            sum += parseInt(marked[i].innerText);
+            mouseBox.innerText = "Dez:" + sum.toString() + "\r\n" + "hexDez:" + sum.toString(16);
+        }
+
+        if (marked.length == 0) {
+            mouseBox.style.display = "none";
+        }
+
+        else {
+            mouseBox.style.display = "inline-block";
+        }
+
+    }
+}); 
