e5419cca2bc93ae60d2d9add07bb7e2cf35bb507
Nadia <Nadia@LAPTOP-07JUC4CA.hfu-open.fu.hs-furtwangen.de>
2017-05-21 14:41:17 +0200

    Signed-off-by: Nadia <Nadia@LAPTOP-07JUC4CA.hfu-open.fu.hs-furtwangen.de>

--- Aufg8/WieseIdeeBienen.js
+++ Aufg8/WieseIdeeBienen.js
@@ -33,7 +33,48 @@ var Aufg8_Wiese;
             }
         }
         drawBiene() {
-            //abtrakt
+            Aufg8_Wiese.inhalt.beginPath();
+            Aufg8_Wiese.inhalt.fillStyle = "black";
+            Aufg8_Wiese.inhalt.strokeStyle = "black";
+            Aufg8_Wiese.inhalt.moveTo(this.x + 2, this.y - 12);
+            Aufg8_Wiese.inhalt.arc(this.x + 2, this.y - 12, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.moveTo(this.x + 9, this.y - 12);
+            Aufg8_Wiese.inhalt.arc(this.x + 9, this.y - 12, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.closePath();
+            Aufg8_Wiese.inhalt.fill();
+            Aufg8_Wiese.inhalt.stroke();
+            Aufg8_Wiese.inhalt.beginPath();
+            Aufg8_Wiese.inhalt.fillStyle = this.farbe;
+            Aufg8_Wiese.inhalt.strokeStyle = this.farbe;
+            Aufg8_Wiese.inhalt.moveTo(this.x + 9, this.y);
+            Aufg8_Wiese.inhalt.arc(this.x + 9, this.y, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.closePath();
+            Aufg8_Wiese.inhalt.fill();
+            Aufg8_Wiese.inhalt.stroke();
+            Aufg8_Wiese.inhalt.beginPath();
+            Aufg8_Wiese.inhalt.fillStyle = "black";
+            Aufg8_Wiese.inhalt.strokeStyle = "black";
+            Aufg8_Wiese.inhalt.moveTo(this.x + 4, this.y);
+            Aufg8_Wiese.inhalt.arc(this.x + 4, this.y, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.closePath();
+            Aufg8_Wiese.inhalt.fill();
+            Aufg8_Wiese.inhalt.stroke();
+            Aufg8_Wiese.inhalt.beginPath();
+            Aufg8_Wiese.inhalt.fillStyle = this.farbe;
+            Aufg8_Wiese.inhalt.strokeStyle = this.farbe;
+            Aufg8_Wiese.inhalt.moveTo(this.x, this.y);
+            Aufg8_Wiese.inhalt.arc(this.x, this.y, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.closePath();
+            Aufg8_Wiese.inhalt.fill();
+            Aufg8_Wiese.inhalt.stroke();
+            Aufg8_Wiese.inhalt.beginPath();
+            Aufg8_Wiese.inhalt.fillStyle = "black";
+            Aufg8_Wiese.inhalt.strokeStyle = "black";
+            Aufg8_Wiese.inhalt.moveTo(this.x - 5, this.y - 2);
+            Aufg8_Wiese.inhalt.arc(this.x - 5, this.y - 2, this.scale, 180, 270);
+            Aufg8_Wiese.inhalt.closePath();
+            Aufg8_Wiese.inhalt.fill();
+            Aufg8_Wiese.inhalt.stroke();
         }
     }
     Aufg8_Wiese.Bee = Bee;
--- Aufg8/WieseIdeeBienen.ts
+++ Aufg8/WieseIdeeBienen.ts
@@ -7,14 +7,14 @@ namespace Aufg8_Wiese {
         scale: number; // Was ist g?
         farbe: string;
 
-
-           
+ 
         constructor(_x: number, _y: number) {
             console.log("Hey, I'm Bob!");
             this.setRandomStyle();
             //this.setRandomPosition();
             this.x = _x;
             this.y = _y;
+             
         }
 
         update(): void {
@@ -50,8 +50,52 @@ namespace Aufg8_Wiese {
         }
 
         drawBiene(): void {
+            inhalt.beginPath();
+            inhalt.fillStyle = "black";
+            inhalt.strokeStyle = "black";
+            inhalt.moveTo(this.x + 2, this.y - 12);
+            inhalt.arc(this.x + 2, this.y - 12, this.scale, 180, 270);
+            inhalt.moveTo(this.x + 9, this.y - 12);
+            inhalt.arc(this.x + 9, this.y - 12, this.scale, 180, 270);
+            inhalt.closePath();
+            inhalt.fill();
+            inhalt.stroke();
 
-            //abtrakt
+            inhalt.beginPath();
+            inhalt.fillStyle = this.farbe;
+            inhalt.strokeStyle = this.farbe;
+            inhalt.moveTo(this.x + 9, this.y);
+            inhalt.arc(this.x + 9, this.y, this.scale, 180, 270);
+            inhalt.closePath();
+            inhalt.fill();
+            inhalt.stroke();
+
+            inhalt.beginPath();
+            inhalt.fillStyle = "black";
+            inhalt.strokeStyle = "black";
+            inhalt.moveTo(this.x + 4, this.y);
+            inhalt.arc(this.x + 4, this.y, this.scale, 180, 270);
+            inhalt.closePath();
+            inhalt.fill();
+            inhalt.stroke();
+
+            inhalt.beginPath();
+            inhalt.fillStyle = this.farbe;
+            inhalt.strokeStyle = this.farbe;
+            inhalt.moveTo(this.x, this.y);
+            inhalt.arc(this.x, this.y, this.scale, 180, 270);
+            inhalt.closePath();
+            inhalt.fill();
+            inhalt.stroke();
+
+            inhalt.beginPath();
+            inhalt.fillStyle = "black";
+            inhalt.strokeStyle = "black";
+            inhalt.moveTo(this.x - 5, this.y - 2);
+            inhalt.arc(this.x - 5, this.y - 2, this.scale, 180, 270);
+            inhalt.closePath();
+            inhalt.fill();
+            inhalt.stroke();
             }
         }
 }
