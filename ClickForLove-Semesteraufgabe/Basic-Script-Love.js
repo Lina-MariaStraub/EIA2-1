3ed12b56680269f94850780eefe1062a4616585a
Nadia <Nadia@192.168.0.95>
2017-07-17 14:57:00 +0200

    Signed-off-by: Nadia <Nadia@192.168.0.95>

--- ClickForLove-Semesteraufgabe/Basic-Script-Love.js
+++ ClickForLove-Semesteraufgabe/Basic-Script-Love.js
@@ -7,9 +7,32 @@ var Love;
         canvas = document.getElementsByTagName("canvas")[0];
         console.log(canvas);
         Love.inhalt = canvas.getContext("2d");
-        let bigHeart = new Love.Heart(750, 355, 10, 0);
+        drawHeart(750, 355, 10, 0);
         //        imgData = inhalt.getImageData(0, 0, canvas.width, canvas.height);        
         //      window.setTimeout(animate, 20);
     } //ende init
+    function drawHeart(_x, _y, _radius, _winkel) {
+        Love.inhalt.moveTo(0, 0);
+        Love.inhalt.beginPath();
+        Love.inhalt.arc(this.x - 3, this.y + 10, this.radius, this.winkel, 2 * Math.PI);
+        Love.inhalt.fillStyle = "red";
+        Love.inhalt.fill();
+        Love.inhalt.closePath();
+        Love.inhalt.moveTo(0, 0);
+        Love.inhalt.beginPath();
+        Love.inhalt.arc(this.x, this.y, this.radius, this.winkel, 2 * Math.PI);
+        Love.inhalt.fillStyle = "red";
+        Love.inhalt.fill();
+        Love.inhalt.closePath();
+        Love.inhalt.beginPath();
+        Love.inhalt.fillStyle = " red ";
+        Love.inhalt.strokeStyle = "red";
+        Love.inhalt.moveTo(this.x, this.y);
+        Love.inhalt.lineTo(this.x - 15, this.y + 15);
+        Love.inhalt.lineTo(this.x - 15, this.y - 15);
+        Love.inhalt.closePath();
+        Love.inhalt.fill();
+        Love.inhalt.stroke();
+    } //ende drawHeart
 })(Love || (Love = {})); //ende namespace
 //# sourceMappingURL=Basic-Script-Love.js.map
--- ClickForLove-Semesteraufgabe/Basic-Script-Love.ts
+++ ClickForLove-Semesteraufgabe/Basic-Script-Love.ts
@@ -12,7 +12,7 @@ namespace Love {
 
         inhalt = canvas.getContext("2d");
         
-        let bigHeart : Heart = new Heart(750, 355, 10, 0);
+        drawHeart(750, 355, 10, 0);
         
         
 //        imgData = inhalt.getImageData(0, 0, canvas.width, canvas.height);        
@@ -21,7 +21,36 @@ namespace Love {
         
 }//ende init
     
-   
+    function drawHeart(_x: number, _y: number, _radius: number, _winkel: number): void {
+
+
+       inhalt.moveTo(0, 0);
+       inhalt.beginPath();
+       inhalt.arc(this.x - 3, this.y + 10, this.radius, this.winkel, 2 * Math.PI);
+       inhalt.fillStyle = "red";
+       inhalt.fill();
+       inhalt.closePath();
+
+       inhalt.moveTo(0, 0);
+       inhalt.beginPath();
+       inhalt.arc(this.x, this.y, this.radius, this.winkel, 2 * Math.PI);
+       inhalt.fillStyle = "red";
+       inhalt.fill();
+       inhalt.closePath();
+
+       inhalt.beginPath();
+       inhalt.fillStyle = " red ";
+       inhalt.strokeStyle = "red";
+       inhalt.moveTo(this.x, this.y);
+       inhalt.lineTo(this.x - 15, this.y + 15);
+       inhalt.lineTo(this.x - 15, this.y - 15);
+       inhalt.closePath();
+       inhalt.fill();
+       inhalt.stroke();        
+
+
+    }//ende drawHeart
+       
     
     
     }//ende namespace
